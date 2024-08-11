5a4f7e82d194f90ba650da3270a842c8ae2a1f23
Pranav <chhabrapranav2001@gmail.com>
2024-08-11 14:21:28 -0500

    Fixed the statements on home page

--- components/MobileLanding.js
+++ components/MobileLanding.js
@@ -1,21 +1,20 @@
 // components/MobileLanding.js
-import React from 'react';
-import { useRouter } from 'next/router';
-import styles from './MobileLanding.module.scss';
+import React from "react";
+import { useRouter } from "next/router";
+import styles from "./MobileLanding.module.scss";
 
 export default function MobileLanding() {
   const router = useRouter();
 
-
   return (
     <div className={styles.mobileLandingContainer}>
       <div className={styles.welcomeText}>
-        <h1>Welcome to IntelliAid</h1>
-        <p>An AI Customer Support platform</p>
+        <h1>Welcome to TrendyThreads</h1>
+        <p>Your ultimate destination for the latest in fashion and style.</p>
       </div>
       <div className={styles.mobileButtons}>
-        <button onClick={() => router.push('/signin')}>Sign In</button>
-        <button onClick={() => router.push('/signup')}>Create Account</button>
+        <button onClick={() => router.push("/signin")}>Sign In</button>
+        <button onClick={() => router.push("/signup")}>Create Account</button>
       </div>
     </div>
   );
